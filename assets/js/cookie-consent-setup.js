f774f49204f411e9a6a5dd9ed68af4cfdadabb1b
elderdavion <112693685+elderdavion@users.noreply.github.com>
2026-05-12 04:13:40 +0000

    Deploying to gh-pages from @ elderdavion/elderdavion.github.io@d4c0f0fec8a7b59ff726925c335345b6f76a49ab 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://hamednasr.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://elderdavion.github.io/#contact">contact us</a>.'
               }
             ]
           }
